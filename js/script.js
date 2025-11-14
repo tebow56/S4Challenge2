2963f343ce6de6df0ad8a8bce6a9554baca3e01f
tebow56 <158113814+tebow56@users.noreply.github.com>
2025-11-15 00:50:21 +0100

    definitivo

--- js/script.js
+++ js/script.js
@@ -1,21 +1,51 @@
 const characterList = document.getElementById("character-list")
+const prevPage = document.getElementById("prev-page")
+const nextPage = document.getElementById("next-page")
+let page = 1
 
-fetch("https://rickandmortyapi.com/api/character")
+prevPage.addEventListener("click", () => {
+    if (page > 1) {
+        page -= 1
+        console.log(page)
+        characterList.innerHTML =''
+        traerPersonajes(page)
+    
+    }
+})
+
+nextPage.addEventListener("click", () => {
+    if (page < 42) {
+    page += 1;
+    console.log(page)
+    characterList.innerHTML =''
+    traerPersonajes(page)
+    }   
+})
+
+function traerPersonajes(page) {
+let url = "https://rickandmortyapi.com/api/character/?page=" + `${page}`
+    fetch(url)
     .then((response) => {
         if(!response.ok) {
             throw new Error ("No se ha podido llevar a cabo la solicitud")
         }
         return response.json();
     }).then ((data) =>{
+        console.log(data)
         data.results.forEach((character) => {
             characterList.innerHTML += `<li id="${character.id}">
                 <ul>
-                    <li id="imagen"><img src="${character.image}" alt="${character.name}"></li>
-                    <li id="nombre"><p><b>Name:</b>${character.name}</p></li>
-                    <li id="especie"><p><b>Species:</b>${character.species}</p></li>
+                        <li id="imagen"><img src="${character.image}" alt="${character.name}"></li>
+                    <li id="nombre"><p>Name:</p><p>${character.name}</p></li>
+                    <li id="especie"><p>Species:</p><p>${character.species}</p></li>
                 </ul>
             </li>`;
         });
     }).catch ((error) => {
     console.error()
     });
+}
+traerPersonajes()
+
+
+
